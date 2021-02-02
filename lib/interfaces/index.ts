b05f4fbb438c837ebe76973a0db72ff4a049b321
econdepe <econdepe@protonmail.com>
2021-02-02 19:32:35 +0100

    Remove deprecated x-pm-permission in attendees

--- lib/interfaces/index.ts
+++ lib/interfaces/index.ts
@@ -13,9 +13,8 @@ export * from './User';
 export * from './UserSettings';
 export * from './Subscription';
 export * from './config';
-export { GetCanonicalEmails } from './hooks/GetCanonicalEmails';
-export { GetVTimezones } from './hooks/GetVTimezones';
-export { VTimezoneObject } from './hooks/GetVTimezones';
+export * from './hooks/GetCanonicalEmails';
+export * from './hooks/GetVTimezones';
 export * from './Hotkeys';
 
 export interface EncryptionConfig {
