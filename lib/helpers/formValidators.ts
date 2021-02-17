a901cf31afe0083b7a3a85ebb0a4f213d14de46f
mmso <mmso@mmso.se>
2021-02-17 15:25:49 +0100

    Fix email validator

--- lib/helpers/formValidators.ts
+++ lib/helpers/formValidators.ts
@@ -3,4 +3,4 @@ import { validateEmailAddress } from './email';
 
 export const requiredValidator = (value: any) =>
     value === '' || value === undefined || value === null ? c('Error').t`This field is required` : '';
-export const emailValidator = (value: string) => (validateEmailAddress(value) ? c('Error').t`Email is not valid` : '');
+export const emailValidator = (value: string) => (!validateEmailAddress(value) ? c('Error').t`Email is not valid` : '');
