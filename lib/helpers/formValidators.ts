51031d3ce92b739da653ce867f78da83d7d5245e
mmso <mmso@mmso.se>
2021-02-19 18:01:23 +0100

    Add number validator

--- lib/helpers/formValidators.ts
+++ lib/helpers/formValidators.ts
@@ -1,6 +1,8 @@
 import { c } from 'ttag';
 import { validateEmailAddress } from './email';
+import { isNumber } from './validators';
 
 export const requiredValidator = (value: any) =>
     value === '' || value === undefined || value === null ? c('Error').t`This field is required` : '';
 export const emailValidator = (value: string) => (!validateEmailAddress(value) ? c('Error').t`Email is not valid` : '');
+export const numberValidator = (value: string) => (!isNumber(value) ? c('Error').t`Not a valid number` : '');
